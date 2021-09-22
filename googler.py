ee5c365fdcb9d375b709300a5b37565103a16f8d
MrKingHatter <82378871+MrKingHatter@users.noreply.github.com>
2021-09-22 16:38:41 +0200

    Update googler.py

--- googler.py
+++ googler.py
@@ -3,6 +3,16 @@ import gspread
 from oauth2client.client import GoogleCredentials
 
 
-def authenticate() -> gspread.client.Client:
-    auth.authenticate_user()
-    return gspread.authorize(GoogleCredentials.get_application_default())
+class Auth:
+    def __init(self):
+        self.credentials = GoogleCredentials.get_application_default()
+        self.client = None
+        
+    def authenticate(self) -> gspread.client.Client:
+        auth.authenticate_user()
+        client = gspread.authorize(GoogleCredentials.get_application_default())
+        return client
+
+    def re_authenticate() -> gspread.client.Client:
+        if self.credentials.access_token_expired:
+            client.login()  # refreshes the token
