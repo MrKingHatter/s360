e0cbdc8275269dfdb1829bc4e80a3e75f8d8940e
MrKingHatter <82378871+MrKingHatter@users.noreply.github.com>
2021-10-20 13:23:00 +0200

    Update timer.py

--- timer.py
+++ timer.py
@@ -2,22 +2,22 @@ from time import time, sleep
 from functools import wraps
 
 
-def time_me(f: callable, end: str = '\n'):
+def time_me(end: str = '\n'):
     message = '{name:} took {timing:.2f}s'
 
-    @wraps(f)
-    def __time_me(__f):
+    def __time_me(f):
+        @wraps(f)
         def wrap(*args):
             t = time()
-            result = __f(*args)
-            print(message.format(name=__f.__name__, timing=time() - t), end=end)
+            result = f(*args)
+            print(message.format(name=f.__name__, timing=time() - t), end=end)
             return result
         return wrap
     return __time_me
 
 
 if __name__ == '__main__':
-    @time_me('')
+    @time_me(' ')
     def test(n):
         sleep(n)
 
